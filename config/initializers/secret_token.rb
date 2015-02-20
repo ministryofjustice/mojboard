076bb4980c38a9fc03c9f747ea69ea224fa7a4d9
Evangelos Giataganas <e.giataganas@gmail.com>
2015-02-20 13:27:25 +0000

    set up the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mojboard::Application.config.secret_key_base = '24614eead61fe5d667985032b2267b266d66065cfdb6f5db143ee16c8815278f3f1f0070c7444e1addf8e4d48fd711f4ab8d40263605ba07d82c13ba3144bc39'
