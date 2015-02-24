28567598cde3120724aba50cddb8fb3bd3ad0a99
Evangelos Giataganas <e.giataganas@gmail.com>
2015-02-24 11:50:47 +0000

    impliment sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,28 @@
 class SessionsController < ApplicationController
+  def create
+    email = auth_hash['info']['email']
+    user = User.where(email: email.to_s).first_or_create
+
+    if user
+      session[:user] = user.id
+      redirect_to posts_path
+    else
+      render :failed
+    end
+  end
+
+  def new
+  end
+
+  def destroy
+    session.delete :user
+
+    redirect_to '/'
+  end
+
+protected
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
 end
