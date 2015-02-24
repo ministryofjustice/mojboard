2d48f37d3ff50b134c9a2a87cb9482755714dee8
Evangelos Giataganas <e.giataganas@gmail.com>
2015-02-24 12:01:52 +0000

    update routes and migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Mojboard::Application.routes.draw do
   resources :posts
+  resource :sessions, only: [:new, :create, :destroy]
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150220135802) do
+ActiveRecord::Schema.define(version: 20150224114713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "posts", force: true do |t|
+  create_table "posts", force: :cascade do |t|
     t.string   "posted_by"
     t.string   "headline"
     t.text     "detail"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
