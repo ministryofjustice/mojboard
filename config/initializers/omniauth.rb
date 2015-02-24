1c96e83118ab046c25ffd03764c8f41c97a2fd86
Evangelos Giataganas <e.giataganas@gmail.com>
2015-02-24 11:53:09 +0000

    update omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :gplus, 'GPLUS_CLIENT_ID', 'GPLUS_CLIENT_SECRET',
+  provider :gplus, '637079548319-h6ejhsurn77502ijpkjd27itjp31lg15.apps.googleusercontent.com', '0UD0a9e1V30FJGA5xKtLjjaB',
     scope: 'openid,profile,email'
 end
